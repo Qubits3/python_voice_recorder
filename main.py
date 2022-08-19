69cbdf0ecbf9a766df4b3e13a38c2a005c4fe412
Metin Yıldıran <metin.1464@gmail.com>
2022-08-19 18:38:21 +0300

    Improved UI

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-import tkinter as tk
+import customtkinter
 import threading
 
 import recorder
 
+customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
+
+button_text = "Record"
+
 
 def main():
-    root = tk.Tk()
+    root = customtkinter.CTk()
+    root.geometry("200x120")
     root.title("Sound Recorder")
 
-    canvas = tk.Canvas(root, width=200, height=100)
-    canvas.grid()
+    frame = customtkinter.CTkFrame(master=root)
+    frame.pack(pady=10, padx=10, fill="both", expand=False)
 
-    button = tk.Button(root, command=lambda: threading.Thread(target=recorder.record_audio).start(), text="Record")
-    button.grid()
+    button = customtkinter.CTkButton(master=frame,
+                                     command=switch(),
+                                     text=button_text)
+    button.pack(pady=10, padx=10)
 
-    button = tk.Button(root, command=lambda: threading.Thread(target=recorder.stop_recording()).start(), text="Stop")
-    button.grid()
+    button = customtkinter.CTkButton(master=frame,
+                                     command=lambda: threading.Thread(target=recorder.stop_recording).start(),
+                                     text="Stop")
+    button.pack(pady=10, padx=10)
 
     root.mainloop()
+
+
+def switch():
+    threading.Thread(target=recorder.record_audio).start()
+    global button_text
+    button_text = "Recording..."
 
 
 if __name__ == '__main__':
--- recorder.py
+++ recorder.py
@@ -1,28 +1,32 @@
 import pyaudio
 import wave
+import datetime
+
+now = datetime.datetime.now()
 
 audio = pyaudio.PyAudio()
-stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+stream = audio.open(format=pyaudio.paInt16, channels=2, rate=48000, input=True, frames_per_buffer=1024)
 frames = []
 
 
 def record_audio():
     try:
         while True:
-            data = stream.read(1024)
+            data = stream.read(num_frames=1024, exception_on_overflow=False)
             frames.append(data)
-    except KeyboardInterrupt:
+    except IOError:
         pass
 
 
 def stop_recording():
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
+    if frames.__sizeof__() > 0 and stream.is_active():
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
 
-    sound_file = wave.open("C:\\Users\\Metin\\Desktop\\my_recording.wav", "wb")
-    sound_file.setnchannels(1)
-    sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-    sound_file.setframerate(44100)
-    sound_file.writeframes(b''.join(frames))
-    sound_file.close()
+        sound_file = wave.open("C:\\Users\\Metin\\Desktop\\" + str(now.strftime("%d-%b-%Y (%H.%M.%S)")) + ".wav", "wb")
+        sound_file.setnchannels(2)
+        sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+        sound_file.setframerate(48000)
+        sound_file.writeframes(b''.join(frames))
+        sound_file.close()
